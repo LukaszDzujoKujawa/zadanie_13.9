7aa81f497e220c765f1c4584b147d5c6e8a6b84f
Lukasz Kujawa <lukasz.kujawa@interia.pl>
2018-06-27 21:47:17 +0200

    add changes JS

--- modules/handlers.js
+++ modules/handlers.js
@@ -8,24 +8,13 @@ var readDir;
 exports.upload = function(request, response) {
 	console.log("Rozpoczynam obsługę żądania upload.");
 	var form = new formidable.IncomingForm();
-	form.uploadDir = "./uploaded_files"; 
-	form.keepExtensions = true;
 	form.parse(request, function(error, fields, files){
-		
-		urlFile = "uploaded_files/" + files.upload.name;
-		urlFile = urlFile.toString();
-		
-		mv(files.upload.path, urlFile, function(err)
-		{if(err) throw error;
-		console.log('Files moved succesfully');
-		});
-	fs.readFile('templates/upload.html',function(err, html){
-			response.writeHead('200', {'Content-Type': "text/html; charset=utf-8"});
-			response.write(html);
+		fs.renameSync(files.upload.path, "a.png");
+	
+			response.writeHead(200, {'Content-Type': "text/html; charset=utf-8"});
 			response.write("<img src='/show' />");
 			response.end();
 		});
-	});
 }
 
 exports.welcome = function(request, response) {
@@ -35,7 +24,6 @@ exports.welcome = function(request, response) {
 		response.write(html);
 		response.end();
 	});
-
 }
 
 exports.error = function(request, response) {
@@ -45,8 +33,8 @@ exports.error = function(request, response) {
 }
 
 exports.show = function(request, response) {
-	fs.readFile(urlFile, "binary", function(error, file) {
-		response.writeHead(200, {"Content-Type": "image/jpg"});
+	fs.readFile("a.png", "binary", function(error, file) {
+		response.writeHead(200, {"Content-Type": "image/png"});
 		response.write(file, "binary");
 		response.end();
 	});
